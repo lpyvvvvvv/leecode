8e01b926dbed41301014ad3c37ab175462d111ac
lpyvvvvvv <paying_lee@163.com>
2017-05-02 12:18:47 +0800

    addtwonumber

--- 2_addtwonumber/main.c
+++ 2_addtwonumber/main.c
@@ -2,29 +2,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-/**
-* Note: The returned array must be malloced, assume caller calls free().
-*/
-int* twoSum(int* nums, int numsSize, int target) {
-	int i, j;
-	int v1, v2;
-	for (i = 0; i<numsSize; i++)
-	{
-		for (j = i + 1; j < numsSize; j++)
-		{
-			v1 = *(nums + i);
-			v2 = *(nums + j);
-			if (v1 + v2 == target)
-			{
-				int* p = (int*)malloc(2 * sizeof(int));
-				*p = i;
-				*(p + 1) = j;
-				return p;
-			}
-		}
-	}
-	return NULL;
-}
 
 
 
